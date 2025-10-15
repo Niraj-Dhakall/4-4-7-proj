b23d6cf0d5f345e34a9cf1c2914fb70901d0f4e0
Niraj Dhakal <59345695+Niraj-Dhakall@users.noreply.github.com>
2025-10-15 15:48:51 -0400

    add  api route. add  support. add

---
+++ rpp/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,65 @@
+import NextAuth from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import prisma from '../../../../../lib/prisma'
+declare module 'next-auth' {
+    interface Session {
+        user: {
+            id?: string;
+            name?: string | null;
+            email?: string | null;
+            image?: string | null;
+        };
+    }
+}
+const handler = NextAuth({
+    providers:[
+        CredentialsProvider({
+            name:'Credentials',
+            credentials:{
+                email: {label: "Email", type: "text", placeholder: "grit@umbc.edu"},
+                password: {label: "Password", type: "password"}
+            },
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials?.password){
+                    return null
+                }
+                const student = await prisma.students.findUnique({
+                    where:{email: credentials.email}
+                })
+
+                if(!student){
+                    return null;
+                }
+                // TODO: add password hashing checking
+
+                return{
+                    id: student.id,
+                    email: student.email,
+                    name: student.name,
+                }
+            },
+        })
+    ],
+    callbacks:{
+        async jwt({token, user}){
+            if(user){
+                token.id = user.id
+            }
+            return token
+        },
+        async session({session, token}){
+            if(session.user){
+                session.user.id = token.id as string
+            }
+            return session
+        }
+    },
+    pages: {
+        signIn: '/login',
+    },
+    session: {
+        strategy: "jwt"
+    }
+})
+
+export {handler as GET, handler as POST}
