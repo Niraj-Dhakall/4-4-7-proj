9beccd5c790b3ef2648ff2be6a664296cc7c48ac
Isaiah Ghansam <isaiahghansam2@gmail.com>
2025-12-01 13:41:52 -0500

    suse client;

---
+++ rpp/src/app/joinsection/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+import JoinSection from "@/components/JoinSection";
+export default function joinSection() {
+    return (
+        <div className="flex justify-center h-[100vh] bg-gradient-to-r from-yellow-400 via-amber-400 to-yellow-500 items-center">
+            <JoinSection />
+        </div>
+    );
+}
